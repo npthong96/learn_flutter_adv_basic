d241e77536037307fdfad64847a849756f28bbe3
npthong96 <npthong96@gmail.com>
2025-06-17 20:25:53 +0700

    update screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:adv_basic/start_screen.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
+  runApp(
+    MaterialApp(
       title: 'Basic Flutter App',
       home: Scaffold(
         backgroundColor: Colors.purple,
-        body: StartScreen(),
+        body: Container(
+          decoration: const BoxDecoration(gradient: LinearGradient(colors: [
+            Colors.deepPurple,
+            Colors.purpleAccent,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight)),
+          child: const StartScreen()),
       ),
-    );
-  }
+    )
+  );
 }
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -7,16 +7,27 @@ class StartScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
           children: [
             Image.asset(
               'assets/images/quiz-logo.png',
-              width: 100,
-              height: 100,
+              width: 300,
             ),
-            const SizedBox(height: 20),
-            const Text("Learn Flutter the fun way"),
-            ElevatedButton(onPressed: (){}, child: const Text("Get Started")),
+            const SizedBox(height: 80),
+            const Text("Learn Flutter the fun way!", 
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+              )
+            ),
+            const SizedBox(height: 30),
+            OutlinedButton(
+              onPressed: (){},
+              style: OutlinedButton.styleFrom(
+                foregroundColor: Colors.white,
+              ), 
+              child: const Text("Start Quiz")
+              ),
           ]
         ),
     );
